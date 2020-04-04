a673d0bdff1a2e0012613a731eae47f858688b6a
uyennhi <uyennhinguyentruong@gmail.com>
2020-04-04 21:04:56 +0700

    create the config package

---
+++ src/main/java/com/program/config/WebApplicationInitializer.java
@@ -0,0 +1,27 @@
+package com.program.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.program.config.WebMvcConfig;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] { WebMvcConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+}
