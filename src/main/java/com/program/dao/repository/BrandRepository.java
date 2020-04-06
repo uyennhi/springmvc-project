14b3e6c74a54dd1e032c0d2986a9959700b59716
uyennhi <uyennhinguyentruong@gmail.com>
2020-04-06 11:38:18 +0700

    next chapter

---
+++ src/main/java/com/program/dao/repository/BrandRepository.java
@@ -0,0 +1,19 @@
+package com.program.dao.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.program.entity.Brand;
+
+@Repository("brandRepository")
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+	
+	Brand findByBrandName(String brandName);
+	
+	@Query("SELECT b.brandName FROM Brand b")
+	List<String> getAllBrandName();
+
+}
---
+++ src/main/java/com/program/utility/ResponseModel.java
@@ -0,0 +1,30 @@
+package com.program.utility;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseModel {
+
+	private HttpStatus httpStatus;
+	private String message;
+
+	public ResponseModel(HttpStatus httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
