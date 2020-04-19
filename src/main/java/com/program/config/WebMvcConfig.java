8117ea688ca82cae5fe90740e80b4b73378fc5db
uyennhi <uyennhinguyentruong@gmail.com>
2020-04-19 16:33:37 +0700

    serve static resource images

--- src/main/java/com/program/config/WebMvcConfig.java
+++ src/main/java/com/program/config/WebMvcConfig.java
@@ -3,11 +3,17 @@ package com.program.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.program.*")
 public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/images/**").addResourceLocations("file:/opt/pilot/images/");
+	}
 
 }
